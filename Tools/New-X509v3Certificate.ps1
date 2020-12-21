1c65b45c90c3b202365781ac46eb0d0caec74188
Paul Dash <paul@dash.training>
2020-12-21 14:02:29 +0100

    updated during 10962 Day 1

--- Tools/New-X509v3Certificate.ps1
+++ Tools/New-X509v3Certificate.ps1
@@ -10,14 +10,15 @@
 # Original source: http://social.technet.microsoft.com/wiki/contents/articles/4714.how-to-generate-a-self-signed-certificate-using-powershell.aspx
 
 # Adopted by:      Paul Wojcicki-Jarocki - Paul Dash (paul@pauldash.com)
-# Adopted because: * change to more secure sha256
+# Adopted because: * 
 #                  * commented throughout
+#                  * change to more secure sha256
 #                  * certificate validity period is corrected
 #                  * generated certificate is added to correct stores
 
 
 Write-Warning "This script is provided AS-IS with no warranties and confers no rights."
-Write-Host    "This script sample will generate a self-signed certificates with private key for code signing purposes.`n"
+Write-Host    "This script will generate a self-signed certificates with exportable private key.`n"
 
 $ContextAnswer = Read-Host "Store certificate in the User or Computer store? (U/C)"
 if ($ContextAnswer -eq "U") { 
@@ -35,6 +36,11 @@ if ($ContextAnswer -eq "U") {
 
 # Set certificate Subject name based on user input
 $Subject = Read-Host "Subject of the certificate  "
+
+if (Get-ChildItem "Cert:\$CertStoreLocation\My\" | Where-Object {$_.Subject -eq "CN=$Subject"}) {
+    Write-Warning "Other certificates for that subject exist."
+}
+
 $DistinguishedName = New-Object -ComObject "X509Enrollment.CX500DistinguishedName.1"  
 $DistinguishedName.Encode("CN=$Subject", 0)
 
@@ -50,6 +56,8 @@ $key.MachineContext = $machineContext
 $key.ExportPolicy = 1 # 0 for non-exportable Private Key
 $key.Create()
 
+
+
 # Create OID for code signing
 ### TODO: check out the .NET type System.Security.Cryptography.X509Certificates.X509EnhancedKeyUsageExtension
 $codesigningoid = New-Object -ComObject "X509Enrollment.CObjectId.1"
@@ -63,7 +71,49 @@ $codesigningoid.InitializeFromValue("1.3.6.1.5.5.7.3.3")        # Code Signing
 
 # Add OID to list
 $ekuoids = New-Object -ComObject "X509Enrollment.CObjectIds.1"
-$ekuoids.Add($codesigningoid) 
+$ekuoids.Add($codesigningoid)
+
+
+##################
+<#
+
+# Add OID to list
+$ekuoids = New-Object -ComObject "X509Enrollment.CObjectIds.1"
+
+
+
+@'
+Code Signing
+Server Authentication
+Client Authentication
+Secure Email
+Time Stamping
+Document Signing
+'@
+$ExitSelectingOID = $false
+do {
+    $ekuOID = New-Object -ComObject "X509Enrollment.CObjectId.1"
+    
+    switch (Read-Host -Prompt 'Type choice of Enhanced Key Usage or [D]one')
+    {
+        '1' { $ekuOID.InitializeFromValue("1.3.6.1.5.5.7.3.3") }
+        '2' { $ekuOID.InitializeFromValue("1.3.6.1.5.5.7.3.1") }
+        '3' { $ekuOID.InitializeFromValue("1.3.6.1.5.5.7.3.2") }
+        '4' { $ekuOID.InitializeFromValue("1.3.6.1.5.5.7.3.4") }
+        '5' { $ekuOID.InitializeFromValue("1.3.6.1.5.5.7.3.8") }
+        '6' { $ekuOID.InitializeFromValue("1.3.6.1.4.1.311.10.3.12") }
+        'D' { $ExitSelectingOID = $true }
+        Default { 'Unsupported key usage!' }
+    }
+
+    if ($ekuOID.Value -and ($ekuOID -notin $ekuoids)) {
+        $ekuoids.Add($ekuOID)
+    }
+
+} until ($ExitSelectingOID)
+#>
+
+
 
 # Add list of OIDs to extensions
 $ekuext = New-Object -ComObject "X509Enrollment.CX509ExtensionEnhancedKeyUsage.1"
@@ -96,41 +146,39 @@ $enrollment.InitializeFromRequest($CertReq)
 # Receive requested certificate
 $Cert = $enrollment.CreateRequest(0)
 
-# Install certificate in store
-$enrollment.InstallResponse(4, $Cert, 0, "") 
 
 ### TODO: Add handling if creation fails!
 Write-Host "Certificate creation: $($enrollment.Status.ErrorText)" -ForegroundColor Green
+# Install certificate in store
+$enrollment.InstallResponse(4, $Cert, 0, "") 
 
 # Export certificate to a file
 $FilePath = Read-Host "Directory to store .CER file"
 if (Test-Path -Path $FilePath -PathType Container) {
-    $SignedCert = Get-ChildItem "Cert:\$CertStoreLocation\My\" | Where-Object {$_.Subject -eq "CN=$Subject"}
-    if ($SignedCert.Count -gt 1) {
-        Write-Warning "Certificate export: Multiple certificates exist for subject '$Subject'. None exported."
-        exit
-    }
+    $SignedCert = Get-ChildItem "Cert:\$CertStoreLocation\My\$($SignedCert.Thumbprint)"
     $ExportedCert = Export-Certificate -Cert $SignedCert -FilePath (Join-Path -Path $FilePath -ChildPath "$Subject.cer")
 } else {
-    Write-Warning "Certificate export: Could not write to path $FilePath."
+    Write-Warning "Certificate export: Could not write to path $FilePath. Will not save to the CA and publishers stores."
     exit
 }
 # Verify export
 if (Test-Path -Path $ExportedCert -PathType Leaf) {
     Write-Host "Certificate export: Completed successfully." -ForegroundColor Green
 } else {
-    Write-Warning "Certificate export: File export failed. Will not save to Root CA and Trusted Publishers stores."
+    Write-Warning "Certificate export: File export failed. Will not save to the CA and publishers stores."
     exit
 }
 
-# Import certificate into Root CA and Trusted Publishers
-Import-Certificate -FilePath $ExportedCert -CertStoreLocation "Cert:\$CertStoreLocation\Root" | Out-Null
-Import-Certificate -FilePath $ExportedCert -CertStoreLocation "Cert:\$CertStoreLocation\TrustedPublisher" | Out-Null
-# Verify import
-### TODO: look up Thumbprint instead of doing the Where-Object
-if ((Get-ChildItem "Cert:\$CertStoreLocation\Root\" | Where-Object {$_.Subject -eq "CN=$Subject"}) -and
-(Get-ChildItem "Cert:\$CertStoreLocation\TrustedPublisher\" | Where-Object {$_.Subject -eq "CN=$Subject"})) {
-    Write-Host "Certificate import: Completed successfully." -Fore Green
-}
+if ($SignedCert.EnhancedKeyUsageList.FriendlyName -contains 'Code Signing') {
 
-Write-Host "Done! Use this path for Set-AuthenticodeSignature:`nCert:\$CertStoreLocation\My\$($SignedCert.Thumbprint)"
+    # Import certificate into Root CA (or Intermediate Certification Authorities) and Trusted Publishers
+    Import-Certificate -FilePath $ExportedCert -CertStoreLocation "Cert:\$CertStoreLocation\Root" | Out-Null
+    Import-Certificate -FilePath $ExportedCert -CertStoreLocation "Cert:\$CertStoreLocation\TrustedPublisher" | Out-Null
+    # Verify import
+    if ((Get-ChildItem "Cert:\$CertStoreLocation\Root\$($SignedCert.Thumbprint)") -and
+        (Get-ChildItem "Cert:\$CertStoreLocation\TrustedPublisher\$($SignedCert.Thumbprint)")) {
+            Write-Host "Certificate installation: Completed successfully." -Fore Green
+    }
+
+    Write-Host "Use this path for Set-AuthenticodeSignature:`nCert:\$CertStoreLocation\My\$($SignedCert.Thumbprint)"
+}
