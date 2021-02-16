be4281024a613bd22b17b0adc58a4778482e78e8
Paul Dash <48420791+PaulDash@users.noreply.github.com>
2021-02-16 03:11:56 +0100

    New tool for a friend.

---
+++ Tools/ADMembership.ps1
@@ -0,0 +1,53 @@
+﻿#           _          _       
+#        __| |____ ___| |__    
+#       / _  |__  / __| '_ \           Script: 'ADMembership.ps1'
+#      | (_| |(_| \__ \ | | |          Author: Paul 'Dash'
+#       \__,_\__,_|___/_| |_(_)        E-mail: paul@dash.training
+#       T  R  A  I  N  I  N  G 
+
+#Requires -Modules ActiveDirectory
+
+Param (
+    # Path to user list with a sAMAccountName or DistinguishedName or SID on each line.
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $Path
+)
+
+# Read file in first so we know how many users to process
+$Users = Get-Content -Path $Path
+$Current = 0
+
+# Loop through the user list 
+foreach ($SingleUser in $Users) {
+
+    Write-Progress -Activity 'Getting group membership' -CurrentOperation $SingleUser -PercentComplete ($Current / $Users.Count)
+
+    try {
+        # Validate user DN
+        $SingleDN = (Get-ADUser -Identity $SingleUser -ErrorAction Stop).DistinguishedName
+    } catch {
+        Write-Warning "Couldn't find user with identity '$SingleUser'."
+    }
+
+    # Proceed if user is valid
+    if ($SingleDN) {
+        try {
+            # Try to get group membership
+            # using LDAP_MATCHING_RULE_IN_CHAIN described in
+            # https://docs.microsoft.com/en-us/windows/win32/adsi/search-filter-syntax?redirectedfrom=MSDN#operators
+            $Groups = Get-ADGroup -LDAPFilter ("(member:1.2.840.113556.1.4.1941:={0})" -f $SingleDN) -ErrorAction Stop
+        } catch {
+            Write-Warning "Couldn't get group membership for '$SingleUser'."
+        }
+               
+        # Loop through result to create User / Group pair for output
+        foreach ($SingleGroup in $Groups) {
+            New-Object -TypeName PSObject -Property @{User=$SingleUser;Group=$SingleGroup.Name}
+        }
+
+        # Cleanup before next loop iteration
+        $SingleDN = $null
+    }
+}
