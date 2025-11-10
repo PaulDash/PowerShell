e7755724b70426275dda7daa2ce3e296f172ef9a
Paul Dash <48420791+PaulDash@users.noreply.github.com>
2025-11-10 13:29:47 +0100

    Now chooses drive letter automatically

--- Demos/Azure VPN Certificates Demo.ps1
+++ Demos/Azure VPN Certificates Demo.ps1
@@ -1,9 +1,9 @@
-#           _          _       
-#        __| |____ ___| |__    
+#           _          _
+#        __| |____ ___| |__
 #       / _  |__  / __| '_ \           Script: 'Azure VPN Certificates Demo.ps1'
 #      | (_| |(_| \__ \ | | |          Author: Paul 'Dash'
 #       \__,_\__,_|___/_| |_(_)        E-mail: paul@dash.training
-#       T  R  A  I  N  I  N  G 
+#       T  R  A  I  N  I  N  G
 
 # Creates a self-signed root certificate for signing client certificates
 $cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
@@ -13,11 +13,17 @@ $cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
                                   -CertStoreLocation "Cert:\CurrentUser\My" `
                                   -KeyUsageProperty Sign -KeyUsage CertSign
 
+# Picks drive to save temporary file
+$DriveLetter =  Get-Volume |
+                Where-Object DriveLetter |
+                Sort-Object DriveLetter |
+                Select-Object -Last 1 -ExpandProperty DriveLetter
+
 # Displays above root certificate
 # Copy-and-paste this into the 'Point-to-site configuration' page of the 'Virtual network gateway'
 # in the section 'Root certificates'
-$cert.rawdata | ConvertTo-Base64 -NoLineBreak | Out-File T:\vpn-CA.txt
-notepad.exe T:\vpn-CA.txt
+$cert.rawdata | ConvertTo-Base64 -NoLineBreak | Out-File "$DriveLetter:\vpn-CA.txt"
+notepad.exe "$DriveLetter:\vpn-CA.txt"
 
 # Creates a client certificate that is placed into the current user's Private certificate store
 New-SelfSignedCertificate -Type Custom -DnsName DemoVPN-Client -KeySpec Signature `
